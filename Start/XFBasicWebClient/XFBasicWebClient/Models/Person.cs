c33808b0c0a58163fe15bcc2503c40801d84c9f9
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2018-04-25 20:21:10 +0900

    update start project

--- Start/XFBasicWebClient/XFBasicWebClient/Models/Person.cs
+++ Start/XFBasicWebClient/XFBasicWebClient/Models/Person.cs
@@ -5,11 +5,6 @@ namespace XFBasicWebClient.Models
 {
     public class Person
     {
-        [JsonProperty(PropertyName = "id")]
-        public int Id { get; set; }
-        [JsonProperty(PropertyName = "name")]
-        public string Name { get; set; }
-        [JsonProperty(PropertyName = "birthday")]
-        public DateTimeOffset Birthday { get; set; }
+
     }
 }
