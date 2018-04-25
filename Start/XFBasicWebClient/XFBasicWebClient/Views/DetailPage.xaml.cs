b49f7e64f54e78443ef3c20ffe915e76fa09707f
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2018-04-25 19:27:47 +0900

    re-copy from finish folder

--- Start/XFBasicWebClient/XFBasicWebClient/Views/DetailPage.xaml.cs
+++ Start/XFBasicWebClient/XFBasicWebClient/Views/DetailPage.xaml.cs
@@ -8,9 +8,45 @@ namespace XFBasicWebClient.Views
 {
     public partial class DetailPage : ContentPage
     {
-        public DetailPage()
+        Person _person;
+
+        public DetailPage(Person person)
         {
             InitializeComponent();
+
+            if (person != null)
+                _person = person;
+            else
+                _person = new Person();
+
+            this.BindingContext = _person;
+
+            saveButton.Clicked += async (sender, e) =>
+            {
+                var updatePerson = new Person
+                {
+                    Id = int.Parse(IdData.Text),
+                    Name = NameData.Text,
+                    Birthday = BirthdayData.Date
+                };
+
+                if (updatePerson.Id == 0)
+                {
+                    await WebApiClient.Instance.PostPersonAsync(updatePerson);
+                }
+                else
+                {
+                    await WebApiClient.Instance.UpdatePersonAsync(updatePerson);
+                }
+
+                await Navigation.PopAsync();
+            };
+
+            deleteButton.Clicked += async (sender, e) =>
+            {
+                await WebApiClient.Instance.DeletePersonAsync(_person);
+                await Navigation.PopAsync();
+            };
         }
     }
 }
